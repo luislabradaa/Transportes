9f25bd6bd85e424d5ec31851da587d9fc7203597
maramendoza692 <mariagmenr@gmail.com>
2022-12-07 19:44:37 -0600

    tres metodos

--- src/app/autobus/autobus.component.ts
+++ src/app/autobus/autobus.component.ts
@@ -1,59 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
-import { ResumenViaje } from '../modelos/ResumenViaje';
-import { TransporteService } from '../service/transporte.service';
 
 @Component({
   selector: 'app-autobus',
   templateUrl: './autobus.component.html',
   styleUrls: ['./autobus.component.css']
 })
-export class AutobusComponent {
+export class AutobusComponent implements OnInit {
 
-origen: any[] = [];
-destino: any[] = [];
-viajeForm:FormGroup | undefined;
-
-viaje: ResumenViaje | undefined;
-
-  constructor(
-    private ts: TransporteService,
-    private fb: FormBuilder,
-  ) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.seleccionarOrigen()
-
   }
 
-  createContactForm():FormGroup{
-    return this.viajeForm = this.fb.group({
-        id_origen: [this.viaje?.id_origen],
-        id_destino: [this.viaje?.id_destino],
-        id_paq: [this.viaje?.id_paq],
-        hora:[this.viaje?.hora],
-        fecha: [this.viaje?.fecha],
-        total:[this.viaje?.total],
-
-        
-      });
-    }
-
-  seleccionarOrigen() {
-    
-    this.ts.consultarOrigen().subscribe((data) => {
-    
-      this.origen = data.list;
-    
-    });
-  }
-
-  seleccionarDestino() {
-    
-    this.ts.consultarDestino().subscribe((data) => {
-    
-      this.origen = data.list;
-    
-    });
-  }
 }
